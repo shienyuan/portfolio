fdc5f8bed9911fe480eb9e70840e5d29af403d57
Shien Yuan <shienyuan97@gmail.com>
2023-07-18 16:19:30 +1200

    feat: add prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
+  nitro: {
+    prerender: {
+      crawlLinks: true
+    }
+  },
   modules: [
     '@nuxtjs/eslint-module',
     '@nuxtjs/tailwindcss',
